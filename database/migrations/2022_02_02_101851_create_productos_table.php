6acfed41929c63fa6098352ebd865353dfe73c80
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-02-02 10:48:14 +0000

    starting project

---
+++ database/migrations/2022_02_02_101851_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("sku", 30);
+            $table->float("precio");
+            $table->string("nombre", 30);
+            $table->string("imagen", 60);
+            $table->integer("stock")->default(0);
+            $table->unsignedBigInteger("categoria_id");
+            $table->timestamps();
+
+            $table->foreign('categoria_id')->references("id")->on("categorias")->ondDelete("set null");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
